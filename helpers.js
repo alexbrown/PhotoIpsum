16e467283dbe45c31795f96680c35530bdccd70c
_alex_brown <alex@robertwbrown.net>
2017-11-27 14:05:28 -0500

    Initial commit

---
+++ helpers.js
@@ -0,0 +1,50 @@
+const request = require("request");
+
+const UNSPLASHURL = "https://api.unsplash.com/photos/random"
+const APIKEY = process.env.APIKEY;
+
+function getSizeFromParams(params){
+    let width = params.split("x")[0];
+    let height = params.split("x")[1];
+    let orientation = getOrientationFromSize(width, height);
+    return {width, height, orientation} 
+}
+
+function getOrientationFromSize(width, height){
+    const orientation = {LANDSCAPE : "landscape", PORTRAIT : "portrait", SQUARISH : "squarish"};
+    if(width === height){
+        return orientation.SQUARISH;
+    }else if(width > height) {
+        return orientation.LANDSCAPE;
+    } else {
+        return orientation.PORTRAIT;
+    }
+}
+
+function buildUnsplashURL(size, term){
+    let url = UNSPLASHURL + "?client_id=" + APIKEY + "&w=" + size.width + "&h=" + size.height + "&orientation=" + size.orientation;
+    if(term){
+        url = url + "&query=" + term;
+    }
+    return url;
+}
+
+function getRandomPhoto(size, callback){
+    let url = buildUnsplashURL(size);
+    request(url, (err, res, body) => {
+        let data = JSON.parse(body);
+        callback(data.urls.custom);
+    })
+}
+
+function getPhotoWithTerm(size, term, callback){
+    let url = buildUnsplashURL(size, term);
+    request(url, (err, res, body) => {
+        let data = JSON.parse(body);
+        callback(data.urls.custom);
+    })
+}
+
+module.exports.getSizeFromParams = getSizeFromParams;
+module.exports.getRandomPhoto = getRandomPhoto;
+module.exports.getPhotoWithTerm = getPhotoWithTerm;
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('public'))
+
+const helpers = require("./helpers");
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')));
+
+app.get("/:size", (req, res) => {
+    if(!(/[\d]{3,4}x[\d]{3,4}/i).test(req.params.size)){
+        res.send();
+        return;
+    }
+    let size = helpers.getSizeFromParams(req.params.size);
+    console.log(size);
+    helpers.getRandomPhoto(size, (photo) => {
+        res.writeHead(302, {
+            'Location' : photo
+        })
+        res.end();
+    });
+});
+
+app.get("/:term/:size", (req, res) => {
+    let size = helpers.getSizeFromParams(req.params.size);
+    let term = req.params.term;
+    helpers.getPhotoWithTerm(size, term, (photo) => {
+        res.writeHead(302, {
+            'Location' : photo
+        })
+        res.end();
+    });
+});
+
+app.listen(9000, console.log("Photo Ipsum is running on port 9000!"));
+
+
